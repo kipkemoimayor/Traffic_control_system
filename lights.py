54da7a1b87ac321b10fca25033b1120165663171
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-06-15 10:56:31 +0300

    Created a light control algorthmn

--- lights.py
+++ lights.py
@@ -1,6 +1,16 @@
-a=5
-b=format(a,'b')
-print(b)
-def keny(2a=0):
-    return
-keny(v)
+import time
+def counter(a):
+    coun=0
+    for i in range(a,0,-1):
+        print(i)
+        time.sleep(1)
+while True:
+    for x in range(0,3):
+        print("RED")
+        print("STOP!! pedestrians passing....")
+        (counter(10))
+        time.sleep(0.5)
+        print("Orange")
+        time.sleep(1)
+        print("Green")
+        time.sleep(5)
